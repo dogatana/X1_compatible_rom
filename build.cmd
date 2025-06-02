7d502069f24a1dcf6ed74e33fe591480de8fe7a2
Toshihiko Ichida <dogatana@gmail.com>
2025-06-03 07:39:12 +0900

    ビルド出力が IPLROM-40.X1 のままだったのを修正

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 REM オリジナルと同じ40桁
-ailz80asm -o IPLROM-40.X1 -om bin -gap 0 -lst iplrom-40.lst -f -i X1_compatible_rom.z80
+ailz80asm -o IPLROM.X1 -om bin -gap 0 -lst iplrom-40.lst -f -i X1_compatible_rom.z80
 
 REM Turbo 高解像度 640x400 80桁
 ailz80asm -o IPLROM.X1T -om bin -gap 0 -lst iplrom.lst -dl TURBO -f -i X1_compatible_rom.z80
